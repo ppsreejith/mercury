b2716892a2d26e0e0366b03c08765b2148a0329b
Sreejith Pp <mailz4sreejith@gmail.com>
2018-06-27 08:29:44 +0530

    Made some changes to App.js content to test hot loading

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Changes you make will automatically reload. Ok, now it does!</Text>
+        <Text>Shake your phone to open the developer menu. This is pretty cool!</Text>
       </View>
     );
   }
