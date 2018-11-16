df5731701fcb6994824ec30e199f0c7ad6694149
Sreejith Pp <mailz4sreejith@gmail.com>
2018-11-16 19:29:09 +0530

    Moved bus data to a json file

--- src/reducers/buses/index.js
+++ src/reducers/buses/index.js
@@ -1,26 +1,15 @@
 import Immutable from 'immutable';
+import _ from 'lodash';
 import { createReducer } from '../../utils';
+import busData from '../../utils/bus-data';
 
-const initialState = Immutable.fromJS({
-  1: {
-    id: '1',
-    coordinates: {
-      latitude: 12.931998723107322,
-      longitude: 77.63207674026489
-    },
-    rotation: 60,
-    seats: 30
-  },
-  2: {
-    id: '2',
-    coordinates: {
-      latitude: 12.937164286215944,
-      longitude: 77.62694835662843
-    },
-    rotation: 60,
-    seats: 20
-  }
-});
+const transformedData = _.reduce(
+  busData,
+  (acc, value) => _.set(acc, _.get(value, 'id'), value),
+  {}
+);
+
+const initialState = Immutable.fromJS(transformedData);
 
 const reducers = {
   BUS_LOCATION_UPDATED: (state, { id, coordinates, rotation }) => state.setIn(id, Immutable.fromJS({ id, coordinates, rotation }))
