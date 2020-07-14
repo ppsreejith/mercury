5d8a9c028e0a452bc54b4e4eedef874b25a7b9e4
ppsreejith <1743700+ppsreejith@users.noreply.github.com>
2020-07-15 01:46:54 +0530

    Added script to fix maps dependencies

---
+++ fix-react-native-maps-dependency.sh
@@ -0,0 +1,2 @@
+sed -i '' 's/compileOnly/compile/g' node_modules/react-native-maps/lib/android/build.gradle;
+sed -i '' 's/implementation/compile/g' node_modules/react-native-maps/lib/android/build.gradle;
